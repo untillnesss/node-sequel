9c3b91ff322541e79d18ddc2e0394a24ee1af0a4
Moch Abdullah Said <pvzuntil@gmail.com>
2021-06-13 11:39:56 +0700

    add: route /

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.get('/', (req, res) => {
+    return res.json('masuk pak eko');
+});
+
 app.listen(process.env.APP_PORT, () => console.log('Runing'));
