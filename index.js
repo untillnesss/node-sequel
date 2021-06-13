06557744522746936093eb566d510f36c60f5678
Moch Abdullah Said <pvzuntil@gmail.com>
2021-06-13 11:37:53 +0700

    add: dotenv

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.listen(process.env.APP_PORT, () => console.log('Runing'));
