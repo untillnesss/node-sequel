4df807d5942b69f0359b9f7140a0cfee3eb3556f
Moch Abdullah Said <pvzuntil@gmail.com>
2021-06-13 11:50:03 +0700

    add: post user

--- index.js
+++ index.js
@@ -2,12 +2,28 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const { User } = require('./models');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     return res.json('masuk pak eko');
+});
+
+app.post('/users/create', async (req, res) => {
+    const { name, email, password } = req.body;
+    console.log(req.body)
+
+    try {
+        const newUser = await User.create({
+            name, email, password,
+        });
+
+        return res.json(newUser);
+    } catch (erro) {
+        console.log(erro)
+    }
 });
 
 app.listen(process.env.APP_PORT, () => console.log('Runing'));
